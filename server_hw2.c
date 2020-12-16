dffeaae410372e2492e472061c11cef023dbec30
George Koubaroulis <gkoubaroulis@gmail.com>
2020-12-17 01:31:02 +0200

    V12_17_130

--- server_hw2.c
+++ server_hw2.c
@@ -9,27 +9,33 @@
 #include <arpa/inet.h>
 #define MAXLINE 1000
 int main(int argc, char **argv) {
-int sockfd, n;
-char recvline[MAXLINE - 1];
-struct sockaddr_in servaddr;
-if (argc != 2)
-    { printf("Usage: %s <ipaddress>", argv[0]);
+    int sockfd, n;
+    char recvline[MAXLINE - 1];
+    struct sockaddr_in servaddr;
+    if (argc != 2)
+        { 
+            printf("Usage: %s <ipaddress>", argv[0]);
+            exit(0);
+        }
+    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+        printf("Socket error");
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13);
+    
+    if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+        printf("inet_pton error for %s", argv[1]);
+    
+    if ( connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 )
+        printf("Connect error");
+    
+    while ( (n = read(sockfd, recvline, MAXLINE)) > 0 ) 
+        {
+        recvline[n] = '\0';
+        if ( fputs(recvline, stdout) == EOF )
+            printf("fputs error");
+        }
+    if ( n < 0 )
+        printf("read error\n");
     exit(0);
-    }
-if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
-    printf("Socket error");
-servaddr.sin_family = AF_INET;
-servaddr.sin_port = htons(13);
-if ( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
-    printf("inet_pton error for %s", argv[1]);
-if ( connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0 )
-    printf("Connect error");
-while ( (n = read(sockfd, recvline, MAXLINE)) > 0 ) {
-    recvline[n] = '\0';
-if ( fputs(recvline, stdout) == EOF )
-    printf("fputs error");
-}
-if ( n < 0 )
-    printf("read error\n");
-exit(0);
 }
